d70f0ffc90f8a2576797c3bf9e9385c88e045e9e
Dave Goddard <dave@goddard.id.au>
2018-06-18 17:10:33 -0300

    Generate cf-deps.iso from golang
    - cache releases and make single run
    - allow skip efi build
    - delete old files from cache (and thus not in iso)
    - put stemcell version in release filenames (allows caching to work with stemcell version change)
    - add only build efi image (allows running in parallel on CI)
    - delete bosh-warden-cpi during bosh.yml generation (Both bosh-lite.yml and warden/cpi.yml add "bosh-warden-cpi" release)

---
+++ README.md
@@ -0,0 +1,1 @@
+# cfdev-builder
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/dgodd/cfdev-builder/boshdep"
+	"github.com/dgodd/cfdev-builder/cfdep"
+	"github.com/dgodd/cfdev-builder/dep"
+	"github.com/dgodd/cfdev-builder/fs"
+	"github.com/dgodd/cfdev-builder/generic"
+	"github.com/dgodd/cfdev-builder/mysqldep"
+)
+
+func main() {
+	var rootDir, cacheDir, outputDir, cfDeployment, boshDeployment, mysqlDeployment, linuxkit string
+	var skipBuildEfiImage, onlyBuildEfiImage bool
+
+	flag.StringVar(&rootDir, "cfdev-repo", "", "root directory of cfdev repo")
+	flag.StringVar(&cacheDir, "cache", filepath.Join(rootDir, "output/cache"), "directory to cache output")
+	flag.StringVar(&outputDir, "output", filepath.Join(rootDir, "output"), "directory for output files")
+	flag.StringVar(&cfDeployment, "cf-deployment", filepath.Join(rootDir, "../cf-deployment"), "input directory for cf-deployment")
+	flag.StringVar(&boshDeployment, "bosh-deployment", filepath.Join(rootDir, "../bosh-deployment"), "input directory for bosh-deployment")
+	flag.StringVar(&mysqlDeployment, "mysql-deployment", filepath.Join(rootDir, "../cf-mysql-deployment"), "input directory for cf-mysql-deployment")
+	flag.BoolVar(&skipBuildEfiImage, "skip-build-efi-image", false, "skip building cfdev-efi.iso")
+	flag.BoolVar(&onlyBuildEfiImage, "only-build-efi-image", false, "only build cfdev-efi.iso")
+	flag.StringVar(&linuxkit, "linuxkit", "linuxkit", "path to linuxkit")
+	flag.Parse()
+
+	if rootDir == "" {
+		var err error
+		rootDir, err = os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if !onlyBuildEfiImage {
+		stemcellVersion, err := dep.StemcellVersion(filepath.Join(cfDeployment, "cf-deployment.yml"))
+		if err != nil {
+			panic(fmt.Errorf("stemcellVersion: %s", err))
+		}
+
+		fs, err := fs.New(cacheDir)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("=== DOWNLOAD STEMCELL")
+		if err := dep.DownloadStemcell(stemcellVersion, fs); err != nil {
+			panic(fmt.Errorf("download stemcell: %s", err))
+		}
+
+		fmt.Println("=== GENERATE/DOWNLOAD BOSH DEPS")
+		if err := boshdep.Build(fs, rootDir, boshDeployment, stemcellVersion); err != nil {
+			panic(err)
+		}
+
+		fmt.Println("=== GENERATE/DOWNLOAD CF DEPS")
+		if err := cfdep.Build(fs, rootDir, cfDeployment, stemcellVersion); err != nil {
+			panic(err)
+		}
+
+		fmt.Println("=== GENERATE/DOWNLOAD MYSQL DEPS")
+		if err := mysqldep.Build(fs, rootDir, mysqlDeployment, stemcellVersion); err != nil {
+			panic(err)
+		}
+
+		fmt.Println("=== GENERATE WORKSPACE.TAR")
+		if err := generic.BuildWorkspaceTar(fs, rootDir); err != nil {
+			panic(err)
+		}
+
+		if err := fs.DeleteOld(); err != nil {
+			panic(err)
+		}
+
+		fmt.Println("=== GENERATE CF.ISO")
+		if err := generic.BuildCfDepsIso(cacheDir, outputDir); err != nil {
+			panic(err)
+		}
+	}
+
+	if !skipBuildEfiImage {
+		fmt.Println("=== BUILD EFI IMAGE")
+		if err := generic.BuildEfiImage(linuxkit, filepath.Join(rootDir, "linuxkit"), outputDir); err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println("\n\nNOW, PLEASE GENERATE CF PLUGIN VIA:", filepath.Join(rootDir, "src/code.cloudfoundry.org/cfdev/generate-plugin.sh"))
+}
